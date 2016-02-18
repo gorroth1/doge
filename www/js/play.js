acf152b31ece06bb1376e0907298e4683debd7e2
Gabe Orroth <gorroth1@gmail.com>
2016-02-18 18:38:44 -0500

    save

--- www/js/play.js
+++ www/js/play.js
@@ -2,4 +2,26 @@
 
 'use strict'
 
-var DogeDodge 
+var DogeDodge = {};
+
+DogeDodge.Play = function () {};
+
+DogeDodge.play.prototype = {
+
+  init: function () {
+    console.log("%c~~~Booting the DogeDodge ~~~\n Compliments of Skilstak", )
+                "color:#FFA500;  background:#073642");
+  },
+
+  preload: function () {
+    this.load.image('background', 'assets/background.png');
+  },
+
+  create: function () {
+  this.background = this.add.titleSprite(0,0,320,568,"background");
+  this.background.autoScroll(0-50);
+  this.background.scale.set(1);
+  },
+
+  update: function () {
+  }
